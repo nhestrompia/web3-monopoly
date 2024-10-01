6bedb51278866089cb6bdb7d4baa17100b8f8088
nhestrompia <nhestrompia@gmail.com>
2024-10-02 01:01:20 +0300

    fix: room list modal

--- monopoly/components/RoomList.tsx
+++ monopoly/components/RoomList.tsx
@@ -1,5 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Users } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 
 interface Room {
   id: string;
@@ -24,20 +31,23 @@ export default function RoomList() {
       </h2>
       <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {rooms.map((room) => (
-          <div
-            key={room.id}
-            className="group relative overflow-hidden rounded-none border-4 border-black bg-gradient-to-br from-cyan-400 to-blue-500 p-4 shadow-hard transition-transform hover:-translate-y-1 hover:translate-x-1 hover:shadow-none"
-          >
-            <h3 className="text-2xl font-bold text-black">{room.name}</h3>
-            <div className="mt-2 flex items-center text-lg font-bold text-purple-800">
+          <Card className="group relative" key={room.id}>
+            <CardHeader>
+              <CardTitle>{room.name}</CardTitle>
+            </CardHeader>
+            <CardContent className="mt-2 flex items-center text-lg font-bold text-purple-800">
               <Users className="mr-2 h-6 w-6" />
               {room.players} / {room.maxPlayers}
-            </div>
-            <Button className="mt-4 w-full rounded-none border-2 border-black bg-yellow-300 text-xl font-bold uppercase text-black shadow-hard transition-transform group-hover:-translate-y-1 group-hover:translate-x-1 group-hover:shadow-none">
-              Join Room
-            </Button>
-            <div className="absolute -right-8 -top-8 h-16 w-16 rotate-45 bg-pink-400 shadow-hard transition-transform group-hover:scale-110"></div>
-          </div>
+            </CardContent>
+            <CardFooter className="flex items-center justify-center">
+              <Button
+                variant={"default"}
+                // className="mt-4 w-full rounded-none border-2 border-black bg-yellow-300 text-xl font-bold uppercase text-black shadow-hard transition-transform group-hover:-translate-y-1 group-hover:translate-x-1 group-hover:shadow-none"
+              >
+                Join Room
+              </Button>
+            </CardFooter>
+          </Card>
         ))}
       </div>
     </div>
